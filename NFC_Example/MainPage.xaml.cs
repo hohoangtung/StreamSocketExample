94b9745247e0a7e3b35a497ab026a9ec4e96e4cc
7ung <hohoangtung12a3@gmail.com>
2015-12-15 15:09:54 +0700

    nfc

--- NFC_Example/MainPage.xaml.cs
+++ NFC_Example/MainPage.xaml.cs
@@ -25,51 +25,30 @@ namespace NFC_Example
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        #region Attribute
         private long messageId = -1;
         ProximityDevice proximityDevice;
+        #endregion
+
+        #region Constuctor & OnNavigatedTo
         public MainPage()
         {
             this.InitializeComponent();
-
             this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
 
         }
+        #endregion
 
-        private async void StartClick(object sender, RoutedEventArgs e)
+        #region Event Click
+        private void StartClick(object sender, RoutedEventArgs e)
         {
+            // Bắt đầu bằng cách lấy thiết bị NFC.
             string id = ProximityDevice.GetDeviceSelector();
             proximityDevice = ProximityDevice.GetDefault();
-            DeviceInformation deviceinformation = null;
-            //try
-            //{
-            //    deviceinformation = await DeviceInformation.CreateFromIdAsync(id);
-
-            //}
-            //catch (Exception ex)
-            //{
-            //    this.OutputString(ex.Message);
-            //}
-            //if (deviceinformation != null)
-            //{
-            //    this.OutputString("Devices Found:");
-            //    OutputString(deviceinformation.Name);
-
-            //}
-            //else
-            //{
-            //    OutputString("No Device Found");
-            //}
-
-            //proximityDevice = ProximityDevice.GetDefault();
 
             if (proximityDevice == null)
             {
@@ -77,29 +56,34 @@ namespace NFC_Example
             }
             else
             {
+                // Thêm sự kiện DeviceArrived và DeviceDeparted. 
+                // Hai sự kiện được kích hoạt khi nhận được thiết bị trong vùng hạot động hoặc thiết bị rời khỏi vùng hoạt động
                 proximityDevice.DeviceArrived += DeviceArrived;
                 proximityDevice.DeviceDeparted +=DeviceDeparted;
                 this.OutputString("Tap to connect ready");
+
+                // Liên tục gởi gói tin, vì NFC là kết nối gần nên không cần bảo mật gì mà chỉ cần gửi gói tin đi thuần tuý
+                // Tham số thứ nhất messageType, có dang Windows.*, với * là chuỗi tự định nghĩa. Ngoài ra còn có các format khác.
                 this.proximityDevice.PublishMessage("Windows.demo", "My name is Tung");
             }
-
         }
 
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            this.OutputTextblock.Text = String.Empty;
+        }
+        #endregion
+
+        #region Departed & Arrived Event
         private async void DeviceDeparted(ProximityDevice sender)
         {
+            // Những sự kiện này được kích hoạt trên thread khác nên không thể gán bình thường mà phải dùng Dispatcher
             await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
                 () =>
                 {
                     this.OutputString("Our friend left us");
-                    //if (messageId != -1)
-                    //{
-                    //    this.proximityDevice.StopSubscribingForMessage(messageId);
-
-                    //}
                 });
-            //throw new NotImplementedException();
         }
-
 
         private async void DeviceArrived(ProximityDevice sender)
         {
@@ -107,6 +91,7 @@ namespace NFC_Example
                 () =>
                 {
                     this.OutputString("Someone arrive!!!");
+                    // Nếu muốn nhận được msg giống như trên thì messageType phải giống ở trên
                     messageId = this.proximityDevice.SubscribeForMessage("Windows.demo", messageReceivedHandler);
                 });
            // throw new NotImplementedException();
@@ -122,16 +107,15 @@ namespace NFC_Example
                     this.OutputString("He said that: " + message.DataAsString);
                 });
         }
+        #endregion
 
+        #region Private Method
         // xuất ra màn hình câu msg
         private void OutputString(string msg)
         {
             this.OutputTextblock.Text += msg + "\n";
         }
+        #endregion
 
-        private void ClearClick(object sender, RoutedEventArgs e)
-        {
-            this.OutputTextblock.Text = String.Empty;
-        }
     }
 }
